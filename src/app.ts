e1fe34ccb05b7d4ccfb3acae27ba80b4f77de8b2
Egor Mishin <em@egormishin.com>
2023-03-01 16:52:39 +0100

    App class implemented

---
+++ src/app.ts
@@ -0,0 +1,22 @@
+import express, {Express} from "express"
+import { userRouter } from "./users/users"
+import { Server } from 'http'
+export class  App {
+    app: Express
+    port: number
+    server: Server
+    constructor() {
+        this.app = express()
+        this.port = 8000
+    }
+
+    public useRoutes() {
+        this.app.use('./users', userRouter )
+    }
+    public async init() {
+        this.useRoutes()
+        this.server = this.app.listen(this.port)
+        console.log(`Server is running on http://localhost:${this.port}`)
+
+    }
+}
