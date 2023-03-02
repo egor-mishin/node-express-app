8bd372ecdb0394b4e66bb0ec26756315d1dc4b87
Egor Mishin <em@egormishin.com>
2023-03-02 14:36:56 +0100

    simple DI implemented

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,16 @@
 import express, {Express} from "express"
 import { userRouter } from "./users/users"
 import { Server } from 'http'
+import {LoggerService} from "./logger/logger.service";
 export class  App {
     app: Express
     port: number
     server: Server
-    constructor() {
+    logger: LoggerService
+    constructor(logger: LoggerService) {
         this.app = express()
         this.port = 8000
+        this.logger = logger
     }
 
     public useRoutes() {
@@ -16,7 +19,7 @@ export class  App {
     public async init() {
         this.useRoutes()
         this.server = this.app.listen(this.port)
-        console.log(`Server is running on http://localhost:${this.port}`)
+        this.logger.log(`Server is running on http://localhost:${this.port}`)
 
     }
 }
---
+++ src/logger/logger.service.ts
@@ -0,0 +1,19 @@
+import  { Logger, ILogObj } from 'tslog'
+export class LoggerService {
+    public logger:Logger<ILogObj>
+    constructor() {
+        this.logger = new Logger()
+    }
+
+     log(...args: unknown[]){
+        this.logger.info(...args)
+     }
+
+    warn(...args: unknown[]){
+        this.logger.warn(...args)
+    }
+
+    error(...args: unknown[]){
+        this.logger.error(...args)
+    }
+}
